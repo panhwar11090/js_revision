440bc5c250ca7ab960c9e5d6922591c90f5b85c9
panhwar11090 <127252478+panhwar11090@users.noreply.github.com>
2023-11-07 21:24:44 +0000

    data types

--- basics/test.js
+++ basics/test.js
@@ -1,2 +1,40 @@
-console.log("huzaifa");
-console.log("psuh2");
+// {
+//     console.log( name);
+// }
+
+// let name = "Huziafa Ahmed";// abb error aiga agar ye block se opar hota to thek he
+
+// {
+//     console.log(age);
+// }
+
+// var age =14; // ab ye undefinde dega
+
+
+// var age =14; // ab ye  sahi chale ga
+// {
+//     console.log(age);
+// }
+
+
+// {
+
+    
+//     console.log(age);
+//     let age =11; ab ye error dega agar ye opar hota to chal jata
+// }
+
+
+
+// {
+//     console.log(age);
+
+// }
+// const age = 15; // ye error dega agar opar hota to thek he
+
+
+
+    
+   
+
+
